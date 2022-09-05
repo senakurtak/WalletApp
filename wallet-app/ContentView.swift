4d9800f4644f30e64a235ad459850f5816d2fe9a
senakurtak <zskurtak@gmail.com>
2022-09-05 13:58:17 +0300

    Initial Commit

---
+++ wallet-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wallet-app
+//
+//  Created by Sena Kurtak on 5.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wallet-app/wallet_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallet_appApp.swift
+//  wallet-app
+//
+//  Created by Sena Kurtak on 5.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct wallet_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
